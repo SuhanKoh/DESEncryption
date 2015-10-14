b08e4ce9c9c9a0d543476956605937f4c1e8ef08
Suhan <suhankoh@gmail.com>
2015-10-13 18:12:36 -0700

    Implemented xor function

--- src/com/suhankoh/CBC.java
+++ src/com/suhankoh/CBC.java
@@ -14,37 +14,59 @@ import java.security.spec.InvalidKeySpecException;
  */
 public class CBC {
     public static void main(String[] args) {
-        if(args.length >= 3) {
+        if (args.length >= 3) {
             String encryptOrDecrypt = args[0];
             String secretKey = args[1];
             File file = new File(args[2]);
+            CBC cbc = new CBC();
+            cbc.performCBC(encryptOrDecrypt, secretKey, file);
+        }
+    }
 
-            try {
-                FileOutputStream writer = new FileOutputStream("test2.txt");
+    public void performCBC(String function, String secretKey, File file) {
+        try {
+            FileOutputStream writer = new FileOutputStream("test2.txt");
 
-                DESLibrary des = new DESLibrary(secretKey, "DES");
-                byte[] bFile = new byte[8]; //8 bytes = 64 bits
-                FileInputStream is = new FileInputStream(file);
-                int size = 0;
-                int offset = 0;
-                byte[] desBytes = new byte[8];
-                while((size = is.read(bFile, 0, bFile.length)) != -1){
-                    
-                    desBytes = des.encrypt(bFile);
-                    System.out.println(desBytes);
-                    writer.write(bFile,0,size);
-                    offset += size;
-                }
+            DESLibrary des = new DESLibrary(secretKey, "DES");
+            byte[] bFile = new byte[8]; //8 bytes = 64 bits
+            FileInputStream is = new FileInputStream(file);
+            int readSize = 0;
 
+            int offset = 0;
+            byte[] desBytes = new byte[8];
+            byte[] desBytes2 = new byte[8];
+            byte[] xorResult = new byte[8];
+            byte[] IV = new byte[]{1, 2, 3, 4, 1, 2, 3, 4, 1};
+            while ((readSize = is.read(bFile, 0, bFile.length)) != -1) {
 
-            } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeySpecException e) {
-                e.printStackTrace();
+                xorResult = xor(bFile, IV, readSize);//perform xor
+                desBytes = des.encrypt(bFile); //encrypt
+
+                writer.write(xorResult, 0, readSize);
+                offset += readSize;
             }
 
+
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeySpecException e) {
+            e.printStackTrace();
         }
 
     }
 
+    /**
+     * Take an input byte array, and IV byte array, and will perform XOR on it, and return an array of byte.
+     * @param input byte array of the input
+     * @param IV
+     * @param readSize
+     * @return
+     */
+    public byte[] xor(byte[] input, byte[] IV, int readSize) {
+        byte[] result = new byte[input.length];
+        for (int i = 0; i < readSize; i++) {
+            result[i] = (byte) (input[i] ^ IV[i]);
+        }
+        return result;
+    }
 
     public void useless() {
         byte[] b1 = "TEST".getBytes();

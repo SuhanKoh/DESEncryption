28730e055fc23d92198863265b52a09274919fc1
Suhan <suhankoh@gmail.com>
2015-10-14 01:16:50 -0700

    Need to fix padding and CBC would be done.

--- src/com/suhankoh/CBC.java
+++ src/com/suhankoh/CBC.java
@@ -1,5 +1,8 @@
 package com.suhankoh;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -7,99 +10,149 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 
 /**
  * Created by Suhan on 10/13/15.
  */
 public class CBC {
-    public static void main(String[] args) {
-        if (args.length >= 3) {
-            String encryptOrDecrypt = args[0];
-            String secretKey = args[1];
-            File file = new File(args[2]);
-            CBC cbc = new CBC();
-            cbc.performCBCEncrypt(encryptOrDecrypt, secretKey, file);
+
+    static String plainText = "Lorem ipsum dolor sit amet, consectetur ";
+    public static final String charset = "UTF8";
+    static byte[] IV;
+    DESLibrary des;
+    byte[][] cipherBytes;
+    String[] cipherText;
+    public static void main(String[] args) throws Exception {
+        IV = new byte[8];
+        new SecureRandom().nextBytes(IV);
+
+        String encryptOrDecrypt = "encrypt";
+        String secretKey = "QWERTYUI";
+        CBC cbc = new CBC(secretKey);
+        cbc.performEncryption();
+        cbc.performDecryption();
+    }
+
+    public CBC(String secretKey) throws Exception{
+        des = new DESLibrary(secretKey, "DES");
+        cipherBytes = new byte[5][8];
+    }
+    public void performEncryption() throws Exception{
+        byte[] plainTextByte = plainText.getBytes();
+        byte[] msg = new byte[8];
+        cipherText = new String[plainTextByte.length/8];
+        int blocks = 0;
+        for(int i = 0; i < plainTextByte.length; i++) {
+            if (i%9 == 0) {
+                System.out.println(msg.length + "length msg");
+                byte[] xor = xor(msg, IV);
+                cipherBytes[blocks] = des.encrypt(xor);
+                IV = cipherBytes[blocks];
+                cipherText[blocks] = new String(cipherBytes[blocks]);
+                blocks++;
+            }
+            msg[i%8] = plainTextByte[i];
+        }
+        for(int i = 0; i< cipherText.length; i++) {
+            System.out.println(cipherText[i]);
         }
     }
 
-    public void performCBCEncrypt(String function, String secretKey, File file) {
-        try {
-            FileOutputStream writer = new FileOutputStream("test2.txt");
-
-            DESLibrary des = new DESLibrary(secretKey, "DES");
-            byte[] bFile = new byte[8]; //8 bytes = 64 bits
-            FileInputStream is = new FileInputStream(file);
-            int readSize = 0;
-
-            int offset = 0;
-
-            byte[] cipherText = new byte[8];
-            byte[] result = new byte[8];
-//            String result = "";
-            byte[] xorResult = new byte[8];
-            byte[] IV = new byte[]{1, 2, 3, 4, 1, 2, 3, 4};
-            while ((readSize = is.read(bFile, 0, bFile.length)) != -1) {
-//                if (readSize < 8) {
-//                    for(int i = readSize; i < 8; i++) {
-//                        System.out.println("padd");
-//                        bFile[i] = ' ';
-//                    }
-//                }
-                if (function.equals("encrypt")) {
-
-                    xorResult = xor(bFile, IV, readSize);//perform xor
-                    result = des.encrypt(xorResult); //encrypt
-                } else if (function.equals("decrypt")) {
-                    cipherText = des.decrypt(bFile); //decrypt
-                    result = xor(cipherText, IV, readSize);//perform xor
-                }
-
-                writer.write(result, 0, 8);
-                offset += readSize;
-            }
-            writer.close();
-
-        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeySpecException e) {
-            e.printStackTrace();
+    public void performDecryption() throws Exception {
+        for(int i = 0; i < cipherBytes.length; i++ ) {
+            //byte[] cipherbyte = cipherText[i].getBytes();
+            byte[] plaintext = des.decrypt(cipherBytes[i]);
+            byte[] xor = xor(plaintext, IV);
+            IV = cipherBytes[i];
+            System.out.println(new String(xor));
         }
-
     }
 
     /**
      * Take an input byte array, and IV byte array, and will perform XOR on it, and return an array of byte.
      *
-     * @param input    byte array of the input
+     * @param input byte array of the input
      * @param IV
-     * @param readSize
      * @return
      */
-    public byte[] xor(byte[] input, byte[] IV, int readSize) {
+    public byte[] xor(byte[] input, byte[] IV) {
+        //System.out.println(input.length + "  " + IV.length);
         byte[] result = new byte[input.length];
-        for (int i = 0; i < readSize; i++) {
-            result[i] = (byte) (input[i] ^ IV[i]);
+        for (int i = 0; i < input.length; i++) {
+            result[i] = (byte) ((int) input[i] ^ (int) IV[i]);
         }
         return result;
     }
+}
 
-    public void useless() {
-        byte[] b1 = "TEST".getBytes();
-        byte[] b2 = "keys".getBytes();
-        byte[] b3 = new byte[b1.length];
-        byte[] b4 = new byte[b1.length];
+/*
+    public void performCBCDecrypt(String function, String secretKey, File file) throws IOException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
+        PrintWriter writer = new PrintWriter("test3.txt");
+        byte[] bFile = new byte[8]; //8 bytes = 64 bits
+        FileInputStream is = new FileInputStream(file);
+        int readSize = 0;
+        byte[] xor;
+        byte[] crypt = new byte[8];
+        String result = "";
+        byte[] fakeIV = IV;
+        while ((readSize = is.read(bFile, 0, bFile.length)) != -1) {
+            System.out.println("testde");
+            //padding
+            if (readSize < 8) {
+                for(int i = readSize; i < 8; i++) {
+                    System.out.println("padd2");
+                    bFile[i] = (byte)'\0';
+                }
+            }
+                crypt = des.decrypt(bFile);
+                xor = xor(crypt, fakeIV);
+                fakeIV = crypt;
+                result = new String(xor, charset);
 
-        int i = 0;
-        System.out.println(new String(b1, StandardCharsets.UTF_8) + " " + new String(b2, StandardCharsets.UTF_8) + "\nBytes" + b1.length + " AND " + b2.length);
-        for (byte bytes : b1)
-            b3[i] = (byte) (bytes ^ b2[i++]);
-        System.out.println(new String(b3, StandardCharsets.UTF_8));
-        i = 0;
 
-        for (byte bytes : b3)
-            b4[i] = (byte) (bytes ^ b2[i++]);
+            writer.write(result);
+        }
+        writer.close();
 
-        System.out.println(new String(b4, StandardCharsets.UTF_8) + " AND " + new String(b1, StandardCharsets.UTF_8));
+    }
+    public void performCBCEncrypt(String function, String secretKey, File file) {
+        try {
+            FileOutputStream writer = new FileOutputStream("test2.txt");
+
+            byte[] bFile = new byte[8]; //8 bytes = 64 bits
+            FileInputStream is = new FileInputStream(file);
+            int readSize = 0;
+            byte[] xor;
+            byte[] crypt = new byte[8];
+            byte[] fakeIV = IV;
+            int offset = 0;
+            String result = "";
+            while ((readSize = is.read(bFile, 0, bFile.length)) != -1) {
+                System.out.println("test");
+                //padding
+                if (readSize < 8) {
+                    for(int i = readSize; i < 8; i++) {
+                        System.out.println("padd");
+                        bFile[i] = (byte)'\0';
+                    }
+                }
+                if(function.equals("encrypt")) {
+                    xor = xor(bFile, fakeIV);
+                    crypt = des.encrypt(xor);
+                    fakeIV = crypt;
+//                    result = new String(crypt, charset);
+                }
+
+                writer.write(crypt);
+            }
+            writer.close();
+
+        } catch (InvalidKeyException | IOException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
 
     }
 
-}
+ */
--- src/com/suhankoh/DESLibrary.java
+++ src/com/suhankoh/DESLibrary.java
@@ -1,27 +1,27 @@
 package com.suhankoh;
 
 import javax.crypto.*;
-import javax.crypto.spec.DESKeySpec;
-import java.io.InputStream;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 public class DESLibrary {
 
-    private DESKeySpec desKeySpec;
-    private SecretKey secretKey;
-    private Cipher cipher;
-    private Cipher decipher;
-    private SecretKey mSecretKey;
+    private SecretKeySpec secretKey;
+    Cipher cipher;
+    Cipher decipher;
+    public DESLibrary(String keys, String algorithm) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, UnsupportedEncodingException {
+        byte[] test = {1,2,3,4,5,6,7,8};
+        byte[] keyBytes = new byte[8];
+        secretKey = new SecretKeySpec(test, "DES");
 
-    public DESLibrary(String keys, String algorithm) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException {
-        desKeySpec = new DESKeySpec(keys.getBytes());
+//        secretKey = new SecretKeySpec(test, algorithm);
 
-        secretKey = SecretKeyFactory.getInstance(algorithm).generateSecret(desKeySpec);
-        cipher = Cipher.getInstance(algorithm); // DES/ECB/PKCS5Padding for SunJCE
-        decipher = Cipher.getInstance(algorithm);
-        
+        cipher = Cipher.getInstance("DES");
+        decipher = Cipher.getInstance("DES");
+
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         decipher.init(Cipher.DECRYPT_MODE, secretKey);
 
@@ -32,7 +32,10 @@ public class DESLibrary {
     }
 
     public byte[] decrypt(byte[] bytes) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        return decipher.doFinal(bytes);
+        System.out.println(bytes.length);
+
+        byte[] test = decipher.doFinal(bytes);
+        return test;
     }
 
 }

f9803a5b3e2ed6f0cfec6010e05b96e939f65da1
Suhan <suhankoh@gmail.com>
2015-10-15 02:37:21 -0700

    OFB is working, need to follow the requirement spec for k bits and clean up

--- src/com/suhankoh/OFB.java
+++ src/com/suhankoh/OFB.java
@@ -1,6 +1,7 @@
 package com.suhankoh;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 /**
@@ -9,71 +10,101 @@ import java.util.ArrayList;
 public class OFB {
     private byte[] IV;
     private DESLibrary des;
-    static String plainText = "QWERQWER";
+    static String plainText = "QWERQWEWERQWER";
 
     public static void main(String[] args) throws Exception {
         OFB ofb = new OFB();
-        //ofb.performOFB();
-        char[] a = new char[]{'f'};
-        char[] b = new char[]{'\0'};
-
-        System.out.println(ofb.xor(a, b));
-
+        ofb.performOFB();
     }
 
 
     public void performOFB() throws Exception {
-        IV = new byte[]{1, 2, 3, 4, 5, 6, 7, 8};
+        IV = new byte[8];
+        new SecureRandom().nextBytes(IV);
+        System.out.println("ID311231:" + new String(IV));
+
         String secretKey = "QWERTYUI";
         des = new DESLibrary(secretKey, "DES");
-        byte[] plantTextBytes = plainText.getBytes();
-        byte[] xor = new byte[8];
-        byte[] encryptedIV = des.encrypt(IV); // encrypt K with IV
-        char[] binaryEncryptedIV = toBinary(encryptedIV).toCharArray(); //for XOR1
-        byte[] msg = new byte[8];
-        for (int i = 0; i < plantTextBytes.length; i++) {
-            msg[i%8] = plantTextBytes[i];
-            if (((i + 1) % 8) == 0 && i > 0) {
-//                String msgStr = new String(msg);
-//                char[] msgBinary = toBinary(binaryEncryptedIV).toCharArray(); //for XOR2
-//                xor = xor(binaryEncryptedIV, msgBinary);
 
+
+        byte[] plainTextBytes = plainText.getBytes();
+        char[] xor = new char[0];
+
+        byte[] encryptedIV = des.encrypt(IV); // encrypt K with IV
+
+        String binaryIV = byteToBinary(IV, IV.length);
+
+        char[] binaryEncryptedIV = byteToBinary(encryptedIV, encryptedIV.length).toCharArray(); //for XOR1
+
+        byte[] msg = new byte[8];
+        int msgCount = 0;
+        for (int i = 0; i < plainTextBytes.length; i++) {
+            msg[i % 8] = plainTextBytes[i];
+            if ((i + 1) % 8 == 0) {
+                msgCount = 8;
+            } else {
+                msgCount++;
             }
+            if (((((i + 1) % 8) == 0) || i == plainTextBytes.length - 1) && i > 0) { // 8 byte blocks
+                String binaryMsgStr = byteToBinary(msg, msgCount);
+                msgCount = 0;
+                char[] msgBinary = binaryMsgStr.toCharArray(); //binaries of the msg for XOR2
+                xor = xor(binaryEncryptedIV, msgBinary); //perform XOR
+                String binary = charArrayToString(xor); //change the XOR chars into String
+
+                binaryIV = discardBinary(binaryIV, binaryMsgStr.length(), binaryIV.length()); //getting the IV
+
+                String temp_binaryEncryptedIV = charArrayToString(binaryEncryptedIV);
+                byte[] temp = temp_binaryEncryptedIV.getBytes();
+                temp_binaryEncryptedIV = byteToBinary(temp, temp.length);
+                System.out.println("ID:" + binaryIV);
+
+                binaryIV = binaryIV + discardBinary(temp_binaryEncryptedIV, 0, binaryMsgStr.length());
+                System.out.println("ID:" + new String(IV));
+                IV = binaryToByte(binaryIV);
+                System.out.println("ID:" + new String(IV));
+                encryptedIV = des.encrypt(IV); // encrypt K with IV
+                binaryIV = byteToBinary(IV, IV.length);
+                binaryEncryptedIV = byteToBinary(encryptedIV, encryptedIV.length).toCharArray(); //for XOR1
+
+                System.out.println("the i is:" + i + " binary: " + binary + "and byte:" + binary.length() / 8 + " string: " + binaryMsgStr);
+            }
+
         }
 
-        System.out.println(new String(xor) + " test ");
 
-        String binaries = toBinary(encryptedIV);
+    }
 
-
-        byte[] array = new byte[]{0xa};
-        byte[] shift = bitsShift(array, 2);
-        System.out.println(String.format("%x", shift[0]));
-
-
+    public String charArrayToString(char[] chars) {
+        StringBuilder xorBinary = new StringBuilder(64);
+        for (int j = 0; j < chars.length; j++) {
+            xorBinary.append((int) chars[j]);
+        }
+        return xorBinary.toString();
     }
 
     public String discardBinary(String binary, int from, int to) {
         return binary.substring(from, to);
     }
 
-    public String toBinary(byte[] bytes) {
+    public String byteToBinary(byte[] bytes, int size) {
         StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
-        for (int i = 0; i < Byte.SIZE * bytes.length; i++)
+        System.out.println(size);
+        for (int i = 0; i < Byte.SIZE * size; i++)
             sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
         return sb.toString();
     }
 
     public byte[] binaryToByte(String binaryString) {
         ArrayList<Integer> byteArrayList = new ArrayList<Integer>();
-
+//        System.out.println("BINARY STRING" + binaryString);
         for (String str : binaryString.split("(?<=\\G.{8})")) { //Regex for Continue matching only if \\G.{8} matches on the left, And \\G.{8} matches the first 8 everytime.
-            System.out.println(str);
+//            System.out.println(str);
             byteArrayList.add(Integer.parseInt(str, 2));
         }
         byte[] bytes = new byte[byteArrayList.size()];
         for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) Integer.parseInt(byteArrayList.get(5).toString());
+            bytes[i] = (byte) Integer.parseInt(byteArrayList.get(i).toString());
         }
         return bytes;
     }
@@ -93,34 +124,13 @@ public class OFB {
      * @return
      */
     public char[] xor(char[] input, char[] IV) {
-        //System.out.println(input.length + "  " + IV.length);
-        char[] result = new char[input.length];
-        for (int i = 0; i < input.length; i++) {
-            result[i] = (char) ( input[i] ^  IV[i]);
+        System.out.println(input.length + "  " + IV.length);
+        int minLength = Math.min(input.length, IV.length);
+        char[] result = new char[minLength];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (char) (input[i] ^ IV[i]);
             //System.out.println("Input: " + (int) input[i] + "  IV: " + (int) IV[i] + "  and the result is: " + (int) result[i]);
         }
         return result;
     }
 }
-
-//        byte[] shifted = bitsShift(array, 1);
-//        int sourceByte = 0xFF & (int)(array[0]);  // Convert byte to unsigned int
-
-//        for (byte b : shifted) {
-//            System.out.println(String.format("%x", 0x20));
-//            System.out.println(String.format("%x", b));
-//        }
-//
-//        String test = toBinary(array);
-//        BigInteger t = new BigInteger(test);
-//        BigInteger shiftInt = t.shiftRight(1);
-
-//        Integer.parseInt(hex, 16);
-//        Integer.parseInt(binary, 2);
-
-//        System.out.println(Integer.parseInt(array[0], 2));
-//        System.out.println(Byte.parseByte("11111110", 2));
-//        byte[] b = new byte[1];
-//        b[0] = (byte)Integer.parseInt(byteArrayList.get(5).toString());
-//        System.out.println(String.format("%x", b[0]));  // Outputs [78, 187, 96, 17, 21]
-//String binaryString = "10101010";
